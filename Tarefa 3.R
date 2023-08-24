fcd839b67f0d98c9c44bfdbb9bba47be0a08eb22
anaapontes <ana.cp.martins@unesp.br>
2023-08-24 15:06:43 -0300

    Tarefa 3 - colocando o CEP no formato certo

--- Tarefa 3.R
+++ Tarefa 3.R
@@ -9,9 +9,9 @@ pdf <- pdf_text("cadastro.pdf")
 pdf <- str_split_1(pdf, "\\n")
 pdf
 
-# Tarefa  -----------------------------------------------------------------
+# Tarefa 3 ----------------------------------------------------------------
 
 z <- pdf[grepl("CEP", pdf)] # Extraindo as linhas que possuem "CEP"
 z <- gsub("^(.*?)(CEP: )", "", z) # Eliminando os caracteres antes do "CEP:", deixando só os números
-z <- gsub("-|\\.", "", z) # Retirando os caracteres "-" e "."
+z <- gsub("\\.", "", z) # Retirando o ".", para deixar no formato 00000-000
 z
